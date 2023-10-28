cb182b033fcfeb094f8b2996229e49267f514cb3
figuitiko <frankfigao@gmail.com>
2023-10-28 17:49:11 -0600

    Adding version 14

--- src/components/share/header/Header.tsx
+++ src/components/share/header/Header.tsx
@@ -5,7 +5,7 @@ import NavBar from './NavBar'
 const Header = () => {
   return (
     <header className='flex w-full h-[64px] justify-center items-center bg-primary text-white px-16 '>
-      <div className='flex py-2 w-full gap-4'>
+      <div className='flex py-2 w-full gap-4 max-w-[1142px]'>
         <LogoBox />
         <NavBar />
         <CallToAction />
